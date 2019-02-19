25509185fe33a5504a47ecf241a5bda06ba3f9d4
zitamarc <47746673+zitamarc@users.noreply.github.com>
2019-02-18 18:52:01 -0800

    Create Item.cpp

---
+++ Item.cpp
@@ -0,0 +1,13 @@
+/
+//  Item.cpp
+//  cspc131_spring2019_project1
+//
+//  Created by Marc Zita on 2/18/19.
+//  Copyright © 2019 Marc Zita. All rights reserved.
+//
+
+#include "Item.hpp"
+Item::Item(string newName, int newSellIn, int newQuality)
+: name(newName), sellIn(newSellIn), quality(newQuality) {
+}
+
